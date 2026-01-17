64b9a209c4be44c9883c1151da0302e0767d5927
Rohith Kumar <rohithkumarattoli@gmail.com>
2026-01-17 18:05:17 +0530

    allowed other domains for CORS

--- server/index_v2.js
+++ server/index_v2.js
@@ -21,7 +21,14 @@ app.use(helmet({
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? ['https://runit-p5ah.onrender.com'] 
-    : ['http://localhost:10000', 'http://127.0.0.1:10000'],
+    : [
+        'http://localhost:10000',
+        'http://127.0.0.1:10000',
+        'http://localhost:5500',
+        'http://127.0.0.1:5500',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000'
+      ],
   credentials: true
 }));
 
